94afc73937f5e8515670bd07074d723082086ce7
liujj05 <liujj05@users.noreply.github.com>
2018-12-13 12:00:35 +0800

    Test按钮功能：读取某特定图像并显示

--- ControlPanel_ver01/Form1.Designer.cs
+++ ControlPanel_ver01/Form1.Designer.cs
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Image_res = new System.Windows.Forms.PictureBox();
+            this.Btn_Test = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.listView_Log = new System.Windows.Forms.ListView();
+            this.From = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.To = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Brief = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.Image_res)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Image_res
+            // 
+            this.Image_res.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Image_res.Location = new System.Drawing.Point(3, 3);
+            this.Image_res.Name = "Image_res";
+            this.Image_res.Size = new System.Drawing.Size(516, 374);
+            this.Image_res.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Image_res.TabIndex = 0;
+            this.Image_res.TabStop = false;
+            // 
+            // Btn_Test
+            // 
+            this.Btn_Test.Location = new System.Drawing.Point(552, 403);
+            this.Btn_Test.Name = "Btn_Test";
+            this.Btn_Test.Size = new System.Drawing.Size(245, 44);
+            this.Btn_Test.TabIndex = 1;
+            this.Btn_Test.Text = "Test";
+            this.Btn_Test.UseVisualStyleBackColor = true;
+            this.Btn_Test.Click += new System.EventHandler(this.Btn_Test_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 95F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 250F));
+            this.tableLayoutPanel1.Controls.Add(this.Image_res, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.Btn_Test, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.listView_Log, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 95F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // listView_Log
+            // 
+            this.listView_Log.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.From,
+            this.To,
+            this.Brief});
+            this.listView_Log.Location = new System.Drawing.Point(552, 3);
+            this.listView_Log.Name = "listView_Log";
+            this.listView_Log.Size = new System.Drawing.Size(245, 374);
+            this.listView_Log.TabIndex = 2;
+            this.listView_Log.UseCompatibleStateImageBehavior = false;
+            this.listView_Log.View = System.Windows.Forms.View.Details;
+            // 
+            // From
+            // 
+            this.From.Text = "From";
+            // 
+            // To
+            // 
+            this.To.Text = "To";
+            // 
+            // Brief
+            // 
+            this.Brief.Text = "Brief";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Image_res)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Image_res;
+        private System.Windows.Forms.Button Btn_Test;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListView listView_Log;
+        private System.Windows.Forms.ColumnHeader From;
+        private System.Windows.Forms.ColumnHeader To;
+        private System.Windows.Forms.ColumnHeader Brief;
     }
 }
 
--- ControlPanel_ver01/Form1.cs
+++ ControlPanel_ver01/Form1.cs
@@ -8,13 +8,71 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// ABB 通信
+using ABB.Robotics.Controllers;
+using ABB.Robotics.Controllers.Discovery;
+using ABB.Robotics.Controllers.RapidDomain;
+
 namespace ControlPanel_ver01
 {
     public partial class Form1 : Form
     {
+
+        // 接收ABB信号需要定义的变量
+        private RapidData RD_rt;
+        private RapidData RD_time;
+        private Num ABB_Num_time;
+        private RobTarget ABB_RT_rt;
+        private Task tRob1;
+        // EndOf-接收ABB信号需要定义的变量
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 主窗口大小、位置
+            int Desk_width = Screen.PrimaryScreen.WorkingArea.Width;
+            int Desk_height = Screen.PrimaryScreen.WorkingArea.Height;
+            Width = Desk_width;
+            Height = Desk_height;
+            Location = new Point(0, 0);
+
+            // 尝试连接ABB
+            NetworkScanner networkScanner = new NetworkScanner();
+            networkScanner.Scan();
+            ControllerInfoCollection controllers = networkScanner.Controllers;
+            foreach (ControllerInfo info in controllers)
+            {
+                ListViewItem item = new ListViewItem("PC");
+                item.SubItems.Add("ABB");
+                item.SubItems.Add("Detected");
+                item.Tag = info;
+                listView_Log.Items.Add(item);
+            }
+        }
+
+        private void Btn_Test_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = ".";
+            file.Filter = "所有文件(*.*)|*.*";
+            file.ShowDialog();
+            string pathname = string.Empty;
+            if (file.FileName != string.Empty)
+            {
+                try
+                {
+                    pathname = file.FileName;
+                    Image_res.Load(pathname);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
     }
 }
