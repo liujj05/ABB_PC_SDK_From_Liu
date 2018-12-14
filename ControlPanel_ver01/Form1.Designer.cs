f83272c2247042d55d4abfdaf0d21291da7caf67
liujj05 <liujj05@users.noreply.github.com>
2018-12-14 14:57:34 +0800

    添加了一个Gocator的按钮，加入了部分Gocator的功能。调整了窗口结构

--- ControlPanel_ver01/Form1.Designer.cs
+++ ControlPanel_ver01/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Image_res = new System.Windows.Forms.PictureBox();
             this.Btn_Test = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
@@ -35,6 +36,8 @@
             this.From = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.To = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Brief = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button_Gocator = new System.Windows.Forms.Button();
+            this.timer_MonitorGocator = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.Image_res)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
@@ -44,16 +47,16 @@
             this.Image_res.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Image_res.Location = new System.Drawing.Point(3, 3);
             this.Image_res.Name = "Image_res";
-            this.Image_res.Size = new System.Drawing.Size(516, 374);
+            this.Image_res.Size = new System.Drawing.Size(758, 494);
             this.Image_res.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.Image_res.TabIndex = 0;
             this.Image_res.TabStop = false;
             // 
             // Btn_Test
             // 
-            this.Btn_Test.Location = new System.Drawing.Point(552, 403);
+            this.Btn_Test.Location = new System.Drawing.Point(867, 529);
             this.Btn_Test.Name = "Btn_Test";
-            this.Btn_Test.Size = new System.Drawing.Size(245, 44);
+            this.Btn_Test.Size = new System.Drawing.Size(94, 44);
             this.Btn_Test.TabIndex = 1;
             this.Btn_Test.Text = "Test";
             this.Btn_Test.UseVisualStyleBackColor = true;
@@ -61,13 +64,15 @@
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 95F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 250F));
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
             this.tableLayoutPanel1.Controls.Add(this.Image_res, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.Btn_Test, 2, 2);
-            this.tableLayoutPanel1.Controls.Add(this.listView_Log, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.listView_Log, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button_Gocator, 1, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -75,7 +80,7 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 95F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1064, 577);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
             // listView_Log
@@ -84,10 +89,12 @@
             this.From,
             this.To,
             this.Brief});
+            this.tableLayoutPanel1.SetColumnSpan(this.listView_Log, 3);
             this.listView_Log.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView_Log.Location = new System.Drawing.Point(552, 3);
+            this.listView_Log.Location = new System.Drawing.Point(767, 3);
             this.listView_Log.Name = "listView_Log";
-            this.listView_Log.Size = new System.Drawing.Size(245, 374);
+            this.tableLayoutPanel1.SetRowSpan(this.listView_Log, 2);
+            this.listView_Log.Size = new System.Drawing.Size(294, 520);
             this.listView_Log.TabIndex = 2;
             this.listView_Log.UseCompatibleStateImageBehavior = false;
             this.listView_Log.View = System.Windows.Forms.View.Details;
@@ -104,11 +111,21 @@
             // 
             this.Brief.Text = "Brief";
             // 
+            // button_Gocator
+            // 
+            this.button_Gocator.Location = new System.Drawing.Point(767, 529);
+            this.button_Gocator.Name = "button_Gocator";
+            this.button_Gocator.Size = new System.Drawing.Size(94, 45);
+            this.button_Gocator.TabIndex = 3;
+            this.button_Gocator.Text = "Gocator";
+            this.button_Gocator.UseVisualStyleBackColor = true;
+            this.button_Gocator.Click += new System.EventHandler(this.button_Gocator_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1064, 577);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -128,6 +145,8 @@
         private System.Windows.Forms.ColumnHeader From;
         private System.Windows.Forms.ColumnHeader To;
         private System.Windows.Forms.ColumnHeader Brief;
+        private System.Windows.Forms.Button button_Gocator;
+        private System.Windows.Forms.Timer timer_MonitorGocator;
     }
 }
 
--- ControlPanel_ver01/Form1.cs
+++ ControlPanel_ver01/Form1.cs
@@ -13,13 +13,23 @@ using ABB.Robotics.Controllers;
 using ABB.Robotics.Controllers.Discovery;
 using ABB.Robotics.Controllers.RapidDomain;
 
+// 与C++通信
+using System.Runtime.InteropServices;
+
+// Gocator 通信
+using Lmi3d.GoSdk;
+using Lmi3d.Zen;
+using Lmi3d.Zen.Io;
+using Lmi3d.GoSdk.Messages;
+
+
+// 针对对话框，之前不能放别的类
 
 namespace ControlPanel_ver01
 {
-
     public partial class Form1 : Form
     {
-
+        // =========变量定义=========
         // 接收ABB信号需要定义的变量
         private RapidData RD_rt;
         private RapidData RD_time;
@@ -31,6 +41,45 @@ namespace ControlPanel_ver01
         private bool ABB_Connect_OK = false;
         // EndOf-接收ABB信号需要定义的变量
 
+        // Gocator 通信
+        private const string SENSOR_IP = "192.168.1.10";
+        public GoSystem system;
+        public GoSensor sensor;
+
+        public static void onData(KObject data)
+        {
+            GoDataSet dataSet = (GoDataSet)data;
+            for (UInt32 i = 0; i < dataSet.Count; i++)
+            {
+                GoDataMsg dataObj = (GoDataMsg)dataSet.Get(i);
+                switch (dataObj.MessageType)
+                {
+                    case GoDataMessageType.Stamp:
+                        {
+                            GoStampMsg stampMsg = (GoStampMsg)dataObj;
+                            for (UInt32 j = 0; j < stampMsg.Count; j++)
+                            {
+                                GoStamp stamp = stampMsg.Get(j);
+                                Console.WriteLine("Frame Index = {0}", stamp.FrameIndex);
+                                Console.WriteLine("Time Stamp = {0}", stamp.Timestamp);
+                                Console.WriteLine("Encoder Value = {0}", stamp.Encoder);
+                            }
+                        }
+                        break;
+                }
+            }
+            // Refer to example ReceiveRange, ReceiveProfile, ReceiveMeasurement and ReceiveWholePart on how to receive data
+            Console.WriteLine(Environment.NewLine);
+        }
+        // Endof-Gocator 通信
+
+        // =C++通信=
+        public string strDlgTitle = "CoreAlgorithm"; // 对方窗口
+        IntPtr hwndRecvWindow = IntPtr.Zero; // 接收窗口指针
+        IntPtr hwndSendWindow = IntPtr.Zero; // 发送窗口指针
+        // =Endof C++通信=
+
+        // =======END of 变量定义=======
         public Form1()
         {
             InitializeComponent();
@@ -58,6 +107,21 @@ namespace ControlPanel_ver01
                 listView_Log.Items.Add(item);
                 ABB_Connect_OK = true;
             }
+
+            // 初始化C++接口
+            hwndRecvWindow = ImportFromDLL.FindWindow(null, strDlgTitle);
+            if (hwndRecvWindow == IntPtr.Zero)
+            {
+                Console.WriteLine("请先启动接收消息程序"); // 这个接口可能要修改，程序需要自动启动
+                return;
+            }
+            hwndSendWindow = ImportFromDLL.GetForegroundWindow();
+            if (hwndSendWindow == IntPtr.Zero)
+            {
+                Console.WriteLine("获取自己的窗口句柄失败，请重试");
+                return;
+            }
+
         }
 
         private void Btn_Test_Click(object sender, EventArgs e)
@@ -97,6 +161,71 @@ namespace ControlPanel_ver01
                 db_qua[i] = double.Parse(str_rot_qua[i]);
             }
             return;
+        }
+
+        private void button_Gocator_Click(object sender, EventArgs e)
+        {
+            // 进行Gocator初始化的相关工作
+            KApiLib.Construct();
+            GoSdkLib.Construct();
+            system = new GoSystem();
+            KIpAddress ipAddress = KIpAddress.Parse(SENSOR_IP);
+            sensor = system.FindSensorByIpAddress(ipAddress);
+            GoSetup sensor_setup = sensor.Setup;
+            sensor.Connect();
+            system.EnableData(true);
+            system.SetDataHandler(onData);
+            // 以下代码需要Debug
+            sensor_setup.TriggerGateEnabled = true; // 设定值
+            bool Trigger_gate_bool_flag1 = false;
+            bool Trigger_gate_bool_flag2 = false;
+            Trigger_gate_bool_flag1 = sensor_setup.TriggerGateEnabledSystemValue; // 读取值1
+            Trigger_gate_bool_flag2 = sensor_setup.TriggerGateEnabledUsed; // 读取值2
+            // 以上代码需要Debug
+            system.Start(); // Start() 之后，在对话框情况下其实我们可以启动一个定时器去读Gocator的
+            
+            // 启动定时器 - 以下两个方法等效
+            // timer_MonitorGocator.Enabled = true;
+            timer_MonitorGocator.Start();
+        }
+
+        private void MonitorGocator_Tick(object sender, EventArgs e)
+        {
+            // 该函数由定时器配置，周期性执行
+            // 主要功能是查询Gocator是否仍有数据
+        }
+
+        public class ImportFromDLL
+        {
+            public const int WM_COPYDATA = 0x004A;
+            public const int WM_QUIT = 0x0012;
+            //启用非托管代码   
+            [StructLayout(LayoutKind.Sequential)]
+            public struct COPYDATASTRUCT
+            {
+                public int dwData;    //not used   
+                public int cbData;    //长度   
+                // [MarshalAs(UnmanagedType.LPStr)] // 例程用
+                // public string lpData; // 例程用
+                public int lpData; // 本程序用
+            }
+
+            [DllImport("User32.dll")]
+            public static extern int SendMessage(
+                IntPtr hWnd,     // handle to destination window    
+                int Msg,         // message   
+                IntPtr wParam,    // first message parameter    
+                ref COPYDATASTRUCT pcd // second message parameter    
+            );
+
+            [DllImport("User32.dll", EntryPoint = "FindWindow")]
+            public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+            [DllImport("Kernel32.dll", EntryPoint = "GetConsoleWindow")]
+            public static extern IntPtr GetConsoleWindow();
+
+            [DllImport("user32.dll", EntryPoint = "GetForegroundWindow")]
+            public static extern IntPtr GetForegroundWindow();
         }
     }
 }
